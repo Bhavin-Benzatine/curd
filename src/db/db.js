6c9067515028836fa4b967d037b41dbeef9cbe22
shailaja-benzatine <shailaja@benzatine.com>
2022-10-03 15:57:12 +0530

    first commit

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://localhost:27017/userSchema")
+// mongoose.connect("mongodb://localhost:27017/userSchema")
+mongoose.connect("mongodb+srv://Bhavin:benzatine123@cluster0.cnfcxjm.mongodb.net/userSchema")
 .then(() => {
     console.log("database connected");
 }).catch((err) => {
